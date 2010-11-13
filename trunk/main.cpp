c371a67e8f6636e28cbf1ec24fcf16fce14eb7a6
iu.biryukov <iu.biryukov@8324a1aa-110f-d21b-d079-f78ca25feddb>
2010-11-13 03:13:54 +0000

    Removed tests from main.cpp

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -6,11 +6,5 @@
 
 int main(int argc, char ** argv)
 {
-    LargeInteger int1;
-    LargeInteger int2;
-
-    int1.from_string("1222");
-    int2 = int1;
-    std::cout << int1.to_string() << "\n" << int2.to_string();
     proccess_files("input.txt", "output.txt");
 }
