51bbaf13c40f2e572368a7794fbe77cf99b8c5a5
iu.biryukov <iu.biryukov@8324a1aa-110f-d21b-d079-f78ca25feddb>
2010-11-20 21:00:02 +0000

    Signed integers, substraction, multiplication

--- utils.cpp
+++ utils.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <fstream>
 #include "utils.h"
 #include "largeint.h"
@@ -6,20 +7,20 @@ void eval_expr(LargeInteger & n1, LargeInteger & n2, char op)
 {
     if (op == '+')
         n1.add(n2);
-    //if (op == '-')
-    //    return n1 - n2;
-    //if (op == '*')
-    //    return n1 * n2;
+    if (op == '-')
+        n1.sub(n2);
+    if (op == '*')
+        n1.mul(n2);
     //if (op == '/')
     //    return n1 / n2;
     //return n1;
 }
 
-void proccess_files(char * input_filename, char * output_filename)
+void proccess_files(const std::string & input_filename,
+                    const std::string & output_filename)
 {
-    std::ifstream input_stream(input_filename);
-
-    std::ofstream output_stream(output_filename);
+    std::ifstream input_stream(input_filename.c_str());
+    std::ofstream output_stream(output_filename.c_str());
 
     int line_count = 0;
     input_stream >> line_count;
@@ -35,16 +36,14 @@ void proccess_files(char * input_filename, char * output_filename)
 	LargeInteger n1;
 	LargeInteger n2;
 
-	n1.from_string(s1.c_str());
-	n2.from_string(s2.c_str());
+        n1.from_string(s1);
+        n2.from_string(s2);
+
 	eval_expr(n1, n2, op);
 
-	char * s = n1.to_string();
+        std::string s = n1.to_string();
 
         output_stream << s << "\n";
-	
-	delete [] s;
-
         --line_count;
     }
 }
