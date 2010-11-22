1189ba55930238f50724f151aa60d714b9e5bbd5
iu.biryukov <iu.biryukov@8324a1aa-110f-d21b-d079-f78ca25feddb>
2010-11-22 23:13:08 +0000

    Operator overloading

--- trunk/utils.h
+++ trunk/utils.h
@@ -3,7 +3,7 @@
 #include <string>
 class LargeInteger;
 
-void eval_expr(LargeInteger & n1, LargeInteger & n2, char op);
+LargeInteger eval_expr(LargeInteger & n1, LargeInteger & n2, char op);
 void proccess_files(const std::string & input_filename,
                     const std::string & output_filename);
 
